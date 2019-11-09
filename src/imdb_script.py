b3e8420807f84645cc062d9276cc2bd076355ebf
ZiningZhu <zining@cs.toronto.edu>
2019-11-09 11:12:27 -0500

    Allow multiple rst parses be done simultaneously

--- src/imdb_script.py
+++ src/imdb_script.py
@@ -3,43 +3,48 @@ import shlex
 import subprocess 
 import parse
 import time
+import argparse
 
-def parse_imdb():
-    src_dir = "../texts/IMDB/preprocessed/{}/{}"
-    choice1 = ["test", "train"]
-    choice2 = ["pos", "neg"]
 
-    log_dir = "imdb_log.log"
+def parse_imdb(choice):
+    src_dir = "../texts/IMDB/preprocessed/{}"
+
+    log_dir = "imdb_log_{}.log".format(choice)
     if os.path.exists(log_dir):
         os.remove(log_dir)
     flog = open(log_dir, "a+")
 
-    for choice1 in ["test", "train"]:
-        for choice2 in ["pos", "neg"]:
-            for fname in os.listdir(src_dir.format(choice1, choice2)):
-                start_time = time.time()
-                full_fn = os.path.join(src_dir.format(choice1, choice2), fname)
-                output_dir = "IMDB/{}/{}".format(choice1, choice2)
-                cmd = "python parse.py -g -t {} {}".format(output_dir, full_fn)
+    for fname in os.listdir(src_dir.format(choice)):
+        start_time = time.time()
+        full_fn = os.path.join(src_dir.format(choice), fname)
+        output_dir = "IMDB/{}".format(choice)
+        cmd = "python parse.py -g -t {} {}".format(output_dir, full_fn)
 
-                pipe = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                try:
-                    stdout, stderr = pipe.communicate(timeout=180)
-                except subprocess.TimeoutExpired:
-                    print ("RST parser timeout: {}".format(full_fn))
-                    flog.write(full_fn + "\n")
-                    flog.flush()
-                    continue
+        pipe = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            stdout, stderr = pipe.communicate(timeout=180)
+        except subprocess.TimeoutExpired:
+            print ("RST parser timeout: {}".format(full_fn))
+            flog.write(full_fn + "\n")
+            flog.flush()
+            continue
 
-                if len(stderr) > 0:
-                    print ("Failed: {}".format(full_fn))
-                    flog.write(full_fn + "\n")
-                    flog.flush()
-                else:
-                    print ("Done:   {} in {:.2f} seconds".format(full_fn, time.time() - start_time))
-
+        if len(stderr) > 0:
+            print ("Failed: {}".format(full_fn))
+            flog.write(full_fn + "\n")
+            flog.flush()
+        else:
+            print ("Done:   {} in {:.2f} seconds".format(full_fn, time.time() - start_time))
     
     flog.close()
 
+
 if __name__ == "__main__":
-    parse_imdb()
+    agp = argparse.ArgumentParser()
+    agp.add_argument('--choice', type=str, default='train/pos_0')
+    args = agp.parse_args()
+
+    assert args.choice in [
+        "test/pos", "test/neg", "train/pos", "train_neg"], "Illegal choice of data fold: {}".format(args.choice)
+
+    parse_imdb(args.choice)

448bd1b60775c4e51dfc7280520ff53b94937313
Thomas Huber <thomas.huber@unisg.ch>
2024-11-21 10:16:36 +0100

    runnable example

---
+++ src/example.py
@@ -0,0 +1,35 @@
+from types import SimpleNamespace
+
+import networkx as nx
+from matplotlib import pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
+
+from parse import DiscourseParser
+from trees.extract_metrics import extract_metrics, extract_relation_ngrams
+
+if __name__ == '__main__':
+    verbose = False
+    skip_parsing = False
+    file_list = []
+    output_dir = None
+    global_features = False
+    logging = False
+    save_preprocessed = True
+    parser = DiscourseParser(
+        verbose,
+        skip_parsing,
+        global_features,
+        save_preprocessed
+    )
+    _, G = parser.parse('example.txt')
+    labels = {
+        node: f"{data['concept']}\n{data.get('text', '')}"
+        for node, data in G.nodes(data=True)
+    }
+    plt.figure(figsize=(15, 12))
+    pos = graphviz_layout(G, prog="dot")
+    nx.draw(G, pos, with_labels=True, labels=labels, node_size=3000, font_size=10)
+    plt.show()
+
+    metrics = extract_metrics(G, relation_ngrams=[(1, 2), (3, 4)])
+    print(metrics)
